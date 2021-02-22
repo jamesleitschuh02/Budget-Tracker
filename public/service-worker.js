c277186e6ceec9bb396e4d8d3cfcfc61153c0d59
James Leitschuh <james.leitschuh02@gmail.com>
2021-02-22 17:22:53 -0600

    service worker code started

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+    '/',
+    './assets/css/style.css',
+    './indexedDB.html',
+    './assets/images/icons/icon-192x192.png',
+    './assets/images/icons/icon-512x512.png'
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v2";
+
+//install
