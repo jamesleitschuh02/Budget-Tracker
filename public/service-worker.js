e86ed37177695a3eb44022d138491498da819d64
James Leitschuh <james.leitschuh02@gmail.com>
2021-02-22 18:35:28 -0600

    added all service worker code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+const STATIC_CACHE = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v2";
+
 const FILES_TO_CACHE = [
     '/',
     './assets/css/style.css',
@@ -6,8 +9,6 @@ const FILES_TO_CACHE = [
     './assets/images/icons/icon-512x512.png'
 ];
 
-const STATIC_CACHE = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v2";
 
 //Installation
 self.addEventListener("install",function(evt) {
@@ -38,4 +39,33 @@ self.addEventListener("activate", function(evt) {
         })
     )
     self.clients.claim();
+});
+
+//Enable service worker to intercept network requests
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api')) {
+        console.log('[Service Worker] Fetch (data)', evt.request.url);
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
+        );
+        return;
+    }
+    evt.respondWith(
+        caches.open(STATIC_CACHE).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
 });
